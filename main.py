fff75141a5d44a80595c4e86fb36ebe35ef5d6d5
eugen <demontage09@rambler.ru>
2022-04-03 12:06:08 +0500

    saving data

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import tkinter
 def generate_password():
     print("password generate")
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
     print("save password")
+    with open('datatext.txt', 'a') as f:
+        f.write(f"{input_website1_1.get()},  {input_email1_2.get()}, {input_password1_3.get()} \n")
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
-window.title("Password Generator")
-window.config(padx=20, pady=20)
+window.title("Password Manager")
+window.config(padx=30, pady=30)
 
 canvas = tkinter.Canvas(width=200, height=200)
 picture = tkinter.PhotoImage(file="logo.png")
@@ -24,21 +27,23 @@ label0_3 = tkinter.Label(text="Password:", font=("Arial", 12))
 label0_3.grid(column=0, row=3)
 
 #entry
-input_website1_1 = tkinter.Entry(width=35)
+input_website1_1 = tkinter.Entry(width=40)
 input_website1_1.grid(column=1, row=1, columnspan=2)
+input_website1_1.focus()
 
-input_email1_2 = tkinter.Entry(width=35)
+input_email1_2 = tkinter.Entry(width=40)
 input_email1_2.grid(column=1, row=2, columnspan=2)
+input_email1_2.insert(0, "demontage09@gmail.com")
 
-input_password1_3 = tkinter.Entry(width=18 )
+input_password1_3 = tkinter.Entry(width=25 )
 input_password1_3.grid(column=1, row=3)
 
 #Button
 
-generate_button = tkinter.Button(text="Generate Password", width=14,font=("Arial", 8), command=generate_password)
+generate_button = tkinter.Button(text="Generate Password", width=14,font=("Arial", 8), highlightthickness=0, command=generate_password)
 generate_button.grid(column=2, row=3)
 
-add_button = tkinter.Button(text="Add", width=35, font=("Arial", 8),  command=save_password)
+add_button = tkinter.Button(text="Add", width=40, font=("Arial", 8),  command=save_password)
 add_button.grid(column=1, row=4, columnspan=2, )
 
 window.mainloop()
