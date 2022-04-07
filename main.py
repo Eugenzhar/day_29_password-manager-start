84f88ec42bb5a5e60262d794b6f02f342c0e8e94
eugen <demontage09@rambler.ru>
2022-04-07 14:16:57 +0500

    finished application

--- main.py
+++ main.py
@@ -1,17 +1,67 @@
+import datetime
 import tkinter
+from tkinter import END
+from tkinter import messagebox as mb
+from _datetime import date
+import random
+import pyperclip
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
 def generate_password():
     print("password generate")
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = password_symbols + password_letters + password_numbers
+
+    random.shuffle(password_list)
+
+    # password = ""
+    # for char in password_list:
+    #     password += char
+
+    password = "".join(password_list)
+
+    print(f"Your password is: {password}")
+    input_password1_3.delete(0, END)
+    input_password1_3.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
-    print("save password")
-    with open('datatext.txt', 'a') as f:
-        f.write(f"{input_website1_1.get()},  {input_email1_2.get()}, {input_password1_3.get()} \n")
-    input_website1_1.delete(0, 100)
-    input_password1_3.delete(0, 100)
-    input_website1_1.focus()
+
+    now_date = datetime.datetime.now()
+    print(f"save password on {now_date}")
+    print(type(now_date))
+    website = input_website1_1.get()
+    email = input_email1_2.get()
+    password = input_password1_3.get()
+
+    #mb.showinfo(title="Title", message="Message")
+    if len(website) == 0 or len(password) == 0:
+        mb.showerror(title="Что-то не так", message=f"all fields must be filled")
+    else:
+        is_ok = mb.askokcancel(title=website, message=f"These are details entered: \n"
+                                                             f"Email: {input_email1_2.get()} \n"
+                                                             f"Password: {password}")
+        if is_ok:
+            with open('datatext.txt', 'a') as f:
+                f.write(f"{input_website1_1.get()},  {input_email1_2.get()}, {input_password1_3.get()}, {now_date},  \n")
+            input_website1_1.delete(0, END)
+            input_password1_3.delete(0, END)
+            input_website1_1.focus()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
